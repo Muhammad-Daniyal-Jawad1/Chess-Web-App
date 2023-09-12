02073a85d2b5c8596893fc9fa73dcda2675da106
Muhammad-Daniyal-Jawad1 <daniyaljawad125@gmail.com>
2023-09-12 10:41:05 +0500

    Login and Register Pages Added

---
+++ src/Login.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // have to make backend for this logic
+    console.log('Login clicked');
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form>
+        <div>
+          <label>Email:</label>
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <button type="button" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
