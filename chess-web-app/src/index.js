8ed630d83289e4ae91f694c1e937dcf0817d6114
Muhammad-Daniyal-Jawad1 <daniyaljawad125@gmail.com>
2024-02-22 23:49:30 +0500

    test change

--- chess-web-app/src/index.js
+++ chess-web-app/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+let somevariable =0;
+console.log(somevariable);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
